2f57dceb33c64b6d983c56caf235c24e757db0ab
Vishal bhojwani <vishalbhojwani917@gmail.com>
2026-02-06 10:19:48 +0530

    added CORS middleware

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const apiRoutes = require('./routes');
 const notFoundHandler = require('./middleware/not-found');
 const errorHandler = require('./middleware/error-handler');
@@ -6,6 +7,7 @@ const errorHandler = require('./middleware/error-handler');
 function createApp() {
   const app = express();
 
+  app.use(cors());
   app.use(express.json());
   app.use('/api/v1', apiRoutes);
 
